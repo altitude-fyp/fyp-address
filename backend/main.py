c2771109b891fc7032765dc967d8d86d7527201f
liuzuolin1996 <liuzuolin1996@gmail.com>
2020-08-20 21:36:09 +0800

    updated README for fastapi

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = fastapi.FastAPI()
 @app.get("/")
 async def root():
     return {"message": "hello world"}
-    
-"""
-uvicorn main:app --reload to run server
-"""
+
+@app.get("/")
+async def example():
+    return {"message": "this is an example!"}
