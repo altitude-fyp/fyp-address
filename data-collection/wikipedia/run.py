7e4c7c3691f439945da2caed464dd2cef0e2ff30
liuzuolin1996 <liuzuolin1996@gmail.com>
2020-08-28 18:23:46 +0800

    minor edit for readabiilty

--- data-collection/wikipedia/run.py
+++ data-collection/wikipedia/run.py
@@ -9,16 +9,21 @@ from components.health import *
 from components.demographics import *
 from components.technology import *
 
-data = [
-    # crime
+print("scraping crime data")
+
+crime = [
     get_intentional_homicide_rate(),
     get_prevalence_of_cocaine_use(),
     get_firearm_death_rate(),
     get_prevalence_of_opiates_use(),
     get_suicide_rate(),
-    get_terrorist_incidents(),
+    get_terrorist_incidents()
+]
 
-    # demographics
+print("done scraping crime data")
+print("scraping demographics data")
+
+demographics = [
     get_age_structure(),
     get_homeless_population(),
     get_number_of_births(),
@@ -29,9 +34,13 @@ data = [
     get_labour_force_composition(),
     get_sex_ratio(),
     get_tertiary_education_attainment(),
-    get_traffic_related_death_rate(),
+    get_traffic_related_death_rate()
+]
 
-    # economics
+print("done scraping demographics data")
+print("scraping economics data")
+
+economics = [
     get_dependency_ratio(),
     get_male_female_income_ratio(),
     get_government_bidget_per_capita(),
@@ -41,13 +50,21 @@ data = [
     get_oil_production(),
     get_foreign_exchange_reserves(),
     get_tariff_rate(),
-    get_unemployment_rate(),
+    get_unemployment_rate()
+]
 
-    # environment
+print("done scraping economics data")
+print("scraping environment data")
+
+environment = [
     get_co2_emissions(),
-    get_natural_gas_consumption(),
+    get_natural_gas_consumption()
+]
 
-    # health
+print("done scraping environment data")
+print("scraping health data")
+
+health = [
     get_bmi(),
     get_HIV_AIDS_prevalence(),
     get_infant_and_under_five_mortality_rate(),
@@ -56,13 +73,21 @@ data = [
     get_mortality_rate(),
     get_number_of_physicians(),
     get_proportion_of_population_using_improved_sanitation_facilities(),
-    get_cigarette_consumption_per_capita(),
+    get_cigarette_consumption_per_capita()
+]
 
-    # technology
+print("done scraping health data")
+print("scraping technology data")
+
+technology = [
     get_number_of_internet_hosts(),
-    get_nummber_of_internet_users(),    
+    get_nummber_of_internet_users()    
 
 ]
+
+print("done scraping technology data")
+
+data = crime + demographics + economics + environment + health + technology
 
 import sys
 here = sys.path[0]
