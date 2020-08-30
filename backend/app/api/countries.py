f79c15e24b5154dc8ef7f3f4df2a515bfbbbc161
liuzuolin1996 <liuzuolin1996@gmail.com>
2020-08-30 15:54:55 +0800

    added countries api

--- backend/app/api/countries.py
+++ backend/app/api/countries.py
@@ -3,8 +3,21 @@ from mongodb_helper import *
 
 @app.get("/api/countries")
 def get_countries():
-    return {"countries": "all"}
+
+    db = get_database()
+    aggregate_countries_collection = db["aggregate.countries"]
+
+    out = {}
+    for country in aggregate_countries_collection.find():
+        out[country["_id"]] = country["data"]
+
+    return out 
 
 @app.get("/api/countries/{country_name}")
 def get_countries(country_name: str):
-    return {"countries": country_name}
+
+    db = get_database()
+    aggregate_countries_collection = db["aggregate.countries"]
+
+    out = aggregate_countries_collection.find_one({"_id": country_name})
+    return out
