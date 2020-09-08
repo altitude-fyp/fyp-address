10f5f6dd5fea91b6675dcd5ab077430290211c6d
dyd-x <38830767+dyd-x@users.noreply.github.com>
2020-09-08 10:41:46 +0800

    Included FSI NPL datasets

--- data-collection/IMF/helpers/download_raw_helper.py
+++ data-collection/IMF/helpers/download_raw_helper.py
@@ -101,7 +101,7 @@ chosen_indicators = {'APDREO':['LUR','PCPI_PCH','PCPIE_PCH','NGDP_R_PPP_PC_PCH']
      'FAS':['FCIODD_NUM','FCIOFI_NUM','FCIODU_NUM','FCIODMF_NUM','FCNODC_NUM','FCRODC_PE_NUM' ,'FCRODU_PE_NUM',
             'FCSODCG_GDP_PT' ,'FCBODC_NUM'], 
      'FDI':['FD_FD_IX', 'FD_FIA_IX', 'FD_FID_IX', 'FD_FIE_IX', 'FD_FI_IX','FD_FMA_IX' ,'FD_FMD_IX','FD_FME_IX', 
-            'FD_FM_IX']}
+            'FD_FM_IX'], 'FSI': ['FSANL_PT', 'FSKNL_PT', 'FSBPNL_PT']}
 
 def get_data(chosen_indicators, period, npl_countries):
     all_country = {}
@@ -113,7 +113,6 @@ def get_data(chosen_indicators, period, npl_countries):
             for indicator in indicators:
                 url = 'http://dataservices.imf.org/REST/SDMX_JSON.svc/CompactData/'
                 key = dataflow_id +'/' + period + '.' + country_code +'.' + indicator
-                print(url+key)
                 try:
                     data = requests.get(f'{url}{key}').json()['CompactData']['DataSet']['Series']['Obs']
                     #check empty
@@ -124,6 +123,7 @@ def get_data(chosen_indicators, period, npl_countries):
                             empty = True
                     if empty == False:
                         all_country[country_code][indicator] = data
+                        print(country_code, indicator)
                 except:
                     pass
                 
