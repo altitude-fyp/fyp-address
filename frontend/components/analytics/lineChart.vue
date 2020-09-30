514e81e019be2c9878c975cde050a21b3aeffa7d
Bang <hbtruong.2017@sis.smu.edu.sg>
2020-09-30 13:03:08 +0800

    Temporarily chartjs

--- frontend/components/analytics/lineChart.vue
+++ frontend/components/analytics/lineChart.vue
@@ -13,15 +13,25 @@ export default {
 
   mounted() {  
     let colour_code = ["#2d4059", "#ea5455", "#f07b3f", "#ffd460"]
+    let datasets_arr = []
+    let size_arr = JSON.parse(JSON.stringify(this.chartdata.years)).length
+    for (i=0; i < size_arr; i++) {
+      datasets_arr.push({
+          label: JSON.parse(JSON.stringify(this.chartdata.title)),
+          backgroundColor: '#79f8b6',
+          data: JSON.parse(JSON.stringify(this.chartdata.value))
+      })
+    }
     let obj = {
         labels: JSON.parse(JSON.stringify(this.chartdata.years)),
-        datasets: [
-          {
-            label: JSON.parse(JSON.stringify(this.chartdata.title)),
-            backgroundColor: '#79f8b6',
-            data: JSON.parse(JSON.stringify(this.chartdata.value))
-          }
-        ]
+        // datasets: [
+        //   {
+        //     label: JSON.parse(JSON.stringify(this.chartdata.title)),
+        //     backgroundColor: '#79f8b6',
+        //     data: JSON.parse(JSON.stringify(this.chartdata.value))
+        //   }
+        // ]
+        datasets: datasets_arr
       }
     console.log(obj)
 
