9e76f84f0d7f69873a4a5aa6286be4263be18a1f
Bang <hbtruong.2017@sis.smu.edu.sg>
2020-09-30 15:20:12 +0800

    Auto regression for charts API

--- backend/app/api/charts.py
+++ backend/app/api/charts.py
@@ -4,6 +4,7 @@ from .constants import *
 from typing import List
 from pydantic import BaseModel
 from collections import defaultdict
+from statsmodels.tsa.ar_model import AutoReg
 
 class ItemList(BaseModel):
     countries: List[str]
@@ -45,6 +46,7 @@ def format_chart_output(data_dict):
             obj["title"] = key
             obj["description"] = GRAPH_SHOWN[key]
             for country in value:
+                country = extrapolate(country)
                 year_list = []
                 value_list = []
                 for k,v in country.items():
@@ -55,3 +57,40 @@ def format_chart_output(data_dict):
             result.append(obj)
     return result
 
+def extrapolate(data, desired=[i for i in range(2000,2020)], lag=1):
+    
+    data = {int(k):float(v) for k,v in data.items()}
+    
+    def get_forward():
+        fstart = len(data)
+        fend = len(data) + max(desired) - max(data.keys())-1
+
+        forward_model = AutoReg(list(data.values()),lag)
+        forward_res = forward_model.fit()
+        forward = forward_model.predict(forward_res.params, start=fstart, end=fend)
+        
+        return list(forward)
+
+    def get_backward():
+        bstart = len(data)
+        bend = len(data) + min(data.keys()) - min(desired) -1
+        
+        print(bstart, bend)
+
+        backward_model = AutoReg(list(data.values())[::-1],lag)
+        backward_res = backward_model.fit()
+        backward = backward_model.predict(backward_res.params, start=bstart, end=bend)
+        
+        return list(backward)[::-1]
+    
+    forward, backward = [], []
+    
+    if max(data.keys()) < max(desired):
+        forward = get_forward()
+        
+    if min(data.keys()) > min(desired):
+        backward = get_backward()
+        
+    temp = backward + list(data.values()) + forward
+    
+    return {k:v for k,v in zip(desired, temp)}
