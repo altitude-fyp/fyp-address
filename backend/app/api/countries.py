ae6058392bf2e6994b60461c4d4da34bf73b4a4a
liuzuolin1996 <liuzuolin1996@gmail.com>
2020-10-02 16:42:38 +0800

    updated post api/countries api

--- backend/app/api/countries.py
+++ backend/app/api/countries.py
@@ -16,70 +16,41 @@ def get_countries_():
     """
     return sorted(list(constants.COUNTRIES.keys()))
 
-
-def format_countries_filter(data_dict):
-    data_list = []
-    for key, value in data_dict.items():
-        data_list.append(key)
-    return data_list
-
-
-@app.post("/api/countries/")
-def get_countries_data(items: ItemList):
+def get_country(cname):
     db = get_database()
-    aggregate_countries_collection = db["aggregate.embeddings"]
-    coordinates = get_coordinates()
-    flag = get_flag()
-    out = {"status": "error", "data": {}}
+    out = db["test.aggregate.embeddings"].find_one({"_id": cname})["data"]
+    
+    # out["lat"] = constants.COUNTRIES[cname]["lat"]
+    # out["lon"] = constants.COUNTRIES[cname]["lon"]
+    # out["code"] = constants.COUNTRIES[cname]["code"]
+    # out["flag"] = constants.COUNTRIES[cname]["flag"]
 
-    combined_raw_data_list = []
-    coordinates_list = []
-    flag_list= []
-    for country_name in items.countries:
-        data = aggregate_countries_collection.find_one({"_id": country_name})["data"]
-        combined_raw_data_list.append(data)
-        coordinates_list.append(coordinates[country_name])
-        flag_list.append(flag[country_name])
-
-    dd = defaultdict(list)
-    for d in combined_raw_data_list:
-        for key, value in d.items():
-            dd[key].append(value)
-
-    if data:
-        result = format_countries_data(dd)
-        out["status"] = "success"
-        out["data"]["coordinates"] = coordinates_list
-        out["data"]["flag"] = flag_list
-        out["data"]["filter"] = get_filter_list()
-        out["data"]["top8"] = result[1]
-        out["data"]["items"] = result[0]
     return out
 
-def format_countries_data(data_dict):
-    data_list = []
-    top8_list = []
-    top8_filter = ["Gdp nominal", "Hdi", "Financial Development Index", "Consumer Price Index, All items",
-                   "Population total", "Area total", "Unemployment rate", "Life expectancy (overall)"]
-    master_list = get_master_filter_list()
-    for item in master_list:
-        obj = {}
-        obj["name"] = item
-        obj["value"] = data_dict[item]
-        data_list.append(obj)
+@app.post("/api/countries/")
+def get_countries__(items:ItemList):
+    countries = ["Singapore", "Malaysia"]
 
-        if item in top8_filter:
-            top8_list.append(obj)
-    return [data_list, top8_list]
+    data = {}
 
-def get_coordinates():
-    db = get_database()
-    constant_collection = db["constant"]
-    data = constant_collection.find_one({"_id": "coordinates"})
-    return data["data"]
+    coordinates = [{"lat":constants.COUNTRIES[country]["lat"], "lon": constants.COUNTRIES[country]["lon"]} for country in countries]
+    flags = [constants.COUNTRIES[country]["flag"] for country in countries]
+    
+    countries = {cname:get_country(cname) for cname in countries}
 
-def get_flag():
-    db = get_database()
-    constant_collection = db["constant"]
-    data = constant_collection.find_one({"_id": "flag"})
-    return data["data"]
+    top8cat = [
+        "Gdp nominal", "Hdi", "Financial Development Index", "Consumer Price Index, All items",
+        "Population total", "Gini", "Unemployment rate", "Life expectancy (overall)"
+        ]
+
+    top8 = [{"name":cat, "value":[cdata.get(cat, None) for cname, cdata in countries.items()]} for cat in top8cat]
+    items = [{"name":fname, "value":[countries[cname][fname] for cname in countries]} for fname, fdata in countries["Singapore"].items()]
+
+    data["coordinates"] = coordinates
+    data["flag"] = flags
+    data["filter"] = constants.COUNTRY_FEATURE_CATEGORIES
+    data["top8"] = top8
+    data["item"] = items
+
+    return data
+    
