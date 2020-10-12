f4f9bef789c2ba481480d19a655101de43e08b51
dyd-x <38830767+dyd-x@users.noreply.github.com>
2020-10-12 19:00:46 +0800

    update pickle for npl data

--- backend/scripts/npl_countries.py
+++ backend/scripts/npl_countries.py
@@ -13,17 +13,27 @@ def get_top_npl_countries():
 
     return sorted(npl_data.items(), key=lambda kv: kv[1])[:20]
 
-def get_npl_data(countryname):
-
+def get_all_npl_data():
     db = get_database()
 
     embeddings_collection = db["imf.quarter"]
+    pickle.dump(embeddings_collection, open("pickled/all_npl_data.sav", "wb"))
+    
+    return 1
 
-    for i in embeddings_collection.find():
+def get_npl_data(countryname):
 
+    all_npl_data = pickle.load("pickled/all_npl_data.sav", "rb")
+
+    for i in all_npl_data.find():
         if i["_id"] == countryname:
             return i["data"]
-
+ 
     return 0
+
+
+get_top_npl_countries()
+
+get_all_npl_data()
 
 print(get_npl_data("Singapore"))
