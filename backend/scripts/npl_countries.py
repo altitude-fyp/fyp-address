ddedb2e56ead3d3678de0f1298e4b88d928e7cac
dyd-x <38830767+dyd-x@users.noreply.github.com>
2020-10-12 17:42:14 +0800

    added npl functions

---
+++ backend/scripts/npl_countries.py
@@ -0,0 +1,29 @@
+from helpers.common import *
+from mongodb_helper import *
+
+def get_top_npl_countries():
+    db = get_database()
+    npl_data = {}
+
+    for i in db["aggregate.embeddings"].find():
+        if "Financial, Financial Soundness Indicators, Core Set, Deposit Takers, Asset Quality, Non-performing Loans to Total Gross Loans, Percent" in i["data"]:
+            npl_data[i["_id"]] = i["data"]["Financial, Financial Soundness Indicators, Core Set, Deposit Takers, Asset Quality, Non-performing Loans to Total Gross Loans, Percent"]
+
+    pickle.dump(npl_data, open("pickled/top_npl_countries.sav", "wb"))
+
+    return sorted(npl_data.items(), key=lambda kv: kv[1])[:20]
+
+def get_npl_data(countryname):
+
+    db = get_database()
+
+    embeddings_collection = db["imf.quarter"]
+
+    for i in embeddings_collection.find():
+
+        if i["_id"] == countryname:
+            return i["data"]
+
+    return 0
+
+print(get_npl_data("Singapore"))
