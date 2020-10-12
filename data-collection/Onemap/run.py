11fb975b684151913fed966660768da09f0dfd86
liuzuolin1996 <liuzuolin1996@gmail.com>
2020-10-12 15:21:05 +0800

    updated onemap cleaning

--- data-collection/Onemap/run.py
+++ data-collection/Onemap/run.py
@@ -9,14 +9,15 @@ from mongodb_helper import *
 
 from helpers.population import *
 from helpers.polygons import *
+from helpers.clean import *
 
 def insert_population_function(area, data):
     """
     function to be inserted into get_population_data function 
     so as to insert into db after every area is done pulling
     """
-    print(f"inserting into onemap: {area}" + " "*40, end="\r")
-    mongo_insert({"_id":area, "data":data}, "onemap")
+    print(f"inserting into onemap.raw: {area}" + " "*40, end="\r")
+    mongo_insert({"_id":area, "data":data}, "onemap.raw")
 
 
 if __name__ == "__main__":
@@ -30,8 +31,17 @@ if __name__ == "__main__":
 
     print("\n")
 
-    mongo_clear("onemap")
+    mongo_clear("onemap.raw")
     population_data = get_population_data(insert_population_function)
 
-    print("\ndone\n")
+    print("\nCleaning onemap data\n")
+
+    data = [i for i in get_database()["onemap.raw"].find()]
+    data = clean(data)
     
+    mongo_clear("onemap")
+    for i in data:
+        print(f"inserting into onemap: {i['_id']}" + " "*40, end="\r")
+        mongo_insert(i, "onemap")
+    
+    print("\n\ndone\n")
