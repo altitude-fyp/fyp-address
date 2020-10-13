9960dbc39b9b758d6912e32577ee2a61770def3c
liuzuolin1996 <liuzuolin1996@gmail.com>
2020-10-14 00:04:51 +0800

    added platform autodetection code for scripts/run.py

--- backend/scripts/run.py
+++ backend/scripts/run.py
@@ -1,8 +1,21 @@
+import platform
 import os
 from generate_constants import *
 
 os.system("rm -rf pickled")
 os.system("mkdir pickled")
 
-os.system("python scripts/generate_constants/run.py")
-os.system("python scripts/generate_country_similarity_matrix/run.py")
+commands = [
+    "python3 scripts/generate_constants/run.py",
+    "python3 scripts/generate_country_similarity_matrix/run.py"
+]
+
+for command in commands:
+    """
+    auto-detects platform used, as "python" is used in windows and "python3" is used otherwise
+    """
+    if platform.system() == "Windows":
+        command = command.replace("python3", "python")
+
+    os.system(command)
+
