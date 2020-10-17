3f093c051a0b5c71de9620d0eded2f8afc53e3c5
tanrachess <huirong.tan.2017@smu.edu.sg>
2020-10-17 21:40:06 +0800

    Zuolin to check out the code
    Pushing a faulty non working version up

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,14 +2,9 @@
 
   <!--This is the main page where it is just '/'-->
   <v-app>
-     <google-map :coordinates="coordinates"/>
-     <c-s-v-search/>
-    <v-container>
-      <country-selection-dialog/>
-      <country-statistics/>
-      <feature-selection-panel/>
-    </v-container>
-
+    <google-map :coordinates="coordinates"/>
+    <c-s-v-search/>
+    <google-bottom-sheet/>
   </v-app>
 
 </template>
@@ -17,16 +12,17 @@
 <script>
 
 
-
 import GoogleMap from "@/components/home/GoogleMap";
 import CSVSearch from "@/components/home/CSVSearch";
 import CountrySelectionDialog from "@/components/home/GoogleBottomSheet/components/CountrySelectionDialog";
 import CountryStatistics from "@/components/home/GoogleBottomSheet/components/CountryStatistics";
 import FeatureSelectionPanel from "@/components/home/GoogleBottomSheet/components/FeatureSelectionPanel";
+import GoogleBottomSheet from "@/components/home/GoogleBottomSheet";
 
 
 export default {
   components: {
+    GoogleBottomSheet,
     FeatureSelectionPanel,
     CountryStatistics,
     CountrySelectionDialog,
