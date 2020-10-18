c606232e2a1d6f64e6188835d243606d8b66f0c3
dyd-x <38830767+dyd-x@users.noreply.github.com>
2020-10-18 18:03:11 +0800

    completed clean_charts.py

--- data-collection/run.py
+++ data-collection/run.py
@@ -1,11 +1,11 @@
 import os
 
 cmds = [
-    "python IMF/run.py",
-    "python dbpedia/run.py",
+    # "python IMF/run.py",
+    # "python dbpedia/run.py",
     "python Onemap/run.py",
-    "python wikipedia/run.py"
-    "python data-aggregator/run.py"
+    # "python wikipedia/run.py"
+    # "python data-aggregator/run.py"
 ]
 
 for cmd in cmds:
