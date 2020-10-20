09912f071d1dab76e9b3eb3f019e43a65581abf7
tanrachess <huirong.tan.2017@smu.edu.sg>
2020-10-20 13:41:01 +0800

    Updated
    Rachess Trying to Fix stuff

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,31 +2,20 @@
 
   <!--This is the main page where it is just '/'-->
   <v-app>
-<<<<<<< HEAD
+
 
     <google-map :coordinates="coordinates"/>
-    <!-- <c-s-v-search/> -->
+     <c-s-v-search/>
 
     <google-bottom-sheet
       @changeInSelectedCountries="updateMainCoordinates">
     </google-bottom-sheet>
 
-=======
-    <google-map :coordinates="coordinates"/>
-    <c-s-v-search/>
-    <google-bottom-sheet/>
->>>>>>> 3f093c051a0b5c71de9620d0eded2f8afc53e3c5
   </v-app>
 
 </template>
 
 <script>
-
-<<<<<<< HEAD
-import GoogleMap from "@/components/home/GoogleMap.vue";
-import GoogleBottomSheet from "@/components/home/GoogleBottomSheet/index.vue";
-=======
-
 import GoogleMap from "@/components/home/GoogleMap";
 import CSVSearch from "@/components/home/CSVSearch";
 import CountrySelectionDialog from "@/components/home/GoogleBottomSheet/components/CountrySelectionDialog";
@@ -34,23 +23,17 @@ import CountryStatistics from "@/components/home/GoogleBottomSheet/components/Co
 import FeatureSelectionPanel from "@/components/home/GoogleBottomSheet/components/FeatureSelectionPanel";
 import GoogleBottomSheet from "@/components/home/GoogleBottomSheet";
 
->>>>>>> 3f093c051a0b5c71de9620d0eded2f8afc53e3c5
 
 export default {
 
   components: {
-<<<<<<< HEAD
-    "google-map": GoogleMap,
-    "google-bottom-sheet": GoogleBottomSheet 
-=======
+
     GoogleBottomSheet,
     FeatureSelectionPanel,
     CountryStatistics,
     CountrySelectionDialog,
     CSVSearch,
     GoogleMap,
-
->>>>>>> 3f093c051a0b5c71de9620d0eded2f8afc53e3c5
   },
 
 
@@ -79,7 +62,7 @@ export default {
           lon: cdata.lon
         })
       }
-      
+
       this.coordinates = out
     }
 
