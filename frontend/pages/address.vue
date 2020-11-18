03dd76980faa8dd88d1a7fd38af8182db20a6857
Bang <hbtruong.2017@sis.smu.edu.sg>
2020-11-18 16:13:42 +0800

    Create address analytics result page

---
+++ frontend/pages/address.vue
@@ -0,0 +1,327 @@
+<template>
+  <v-app>
+    <div style="padding-top: 50px">
+      <div class="parent">
+          <div class="left">
+
+            <!-- File Info -->
+            <v-row>
+              <h2 class="headerText">Input Address</h2>
+            </v-row>
+            
+            <v-row>
+              <div class="file">
+                <h3> {{ this.$route.params.address }} </h3>                
+              </div>
+            </v-row><br/>
+
+            <v-row>
+              <span class="uploadDate">Last uploaded on {{ new Date().toLocaleString() }}</span>
+            </v-row>
+            <!-- File Info -->
+
+            <br/><v-divider></v-divider><br/>
+
+            <v-row>
+
+              <h2 class="headerText">Address data</h2>
+              
+              <v-expansion-panels 
+                accordion 
+                v-model="panel"
+                multiple>
+
+                <v-expansion-panel>
+                  <v-expansion-panel-header>
+                    <v-img class="analyticsIcons"
+                      max-height="18"
+                      max-width="18"
+                      :src="require('../images/icons/ataglance.png')"
+                    ></v-img>
+                    <h3>At a Glance</h3>
+                  </v-expansion-panel-header>
+
+                  <v-expansion-panel-content>
+
+                    <v-row class="atAGlanceResults">
+                      <v-col>{{ this.housingType }}</v-col>
+                      <v-col>{{ this.selectedRegions[0] }}</v-col>
+                      <v-col>{{ this.selectedCountries[0] }}</v-col>
+                    </v-row>
+                    <v-row class="atAGlanceSubHeader">
+                      <v-col>Housing Type</v-col>
+                      <v-col>Region</v-col>
+                      <v-col>Country</v-col>
+                    </v-row>
+
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+
+                <v-expansion-panel>
+                  <v-expansion-panel-header>
+                    <v-img class="analyticsIcons"
+                      max-height="18"
+                      max-width="18"
+                      :src="require('../images/icons/globe.png')"
+                    ></v-img>
+                    <h3>Singapore</h3>
+                  </v-expansion-panel-header>
+
+                  <v-expansion-panel-content>
+                      <country-statistics
+                          :countriesMetadata="{1:1}"
+                          :countryStatistics=countryStatistics
+                          :selectedCountries=selectedCountries
+                          :selectedFeatures=selectedCountryStatisticsFeatures>
+                      </country-statistics>
+
+                      <key-financial-indicators-analytics
+                        :chartData=chartData>
+                      </key-financial-indicators-analytics>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+
+                <v-expansion-panel>
+                  <v-expansion-panel-header>
+                    <v-img class="analyticsIcons"
+                      max-height="18"
+                      max-width="18"
+                      :src="require('../images/icons/region.png')"
+                    ></v-img>
+                    <h3>Regions in Singapore</h3>
+                  </v-expansion-panel-header>
+
+                  <v-expansion-panel-content>
+
+                      <tabs v-if="selectedRegions" :selected-regions="selectedRegions"/>
+
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+                 
+              </v-expansion-panels>
+            </v-row>
+          </div>
+
+      <!--Google Maps-->
+      <div class="right">
+        <google-map-analytics :coordinates="coordinates"/>
+      </div>
+      <!--Google Maps-->
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import lineChart from "@/components/analytics/lineChart";
+import BarChart from "@/components/analytics/barChart";
+import GoogleMapAnalytics from "@/components/home/GoogleMapAnalytics";
+import CountryStatistics from "@/components/analytics/CountryStatisticsAnalytics.vue"
+import KeyFinancialIndicatorsAnalytics from "@/components/analytics/KeyFinancialIndicatorsAnalytics.vue"
+import KeyFinancialIndicators from "@/components/home/GoogleBottomSheet/components/KeyFinancialIndicators.vue"
+import tabs from "@/components/region/tabs"
+
+export default {
+  name: "address",
+  components: {
+    BarChart, lineChart, GoogleMapAnalytics,
+    "country-statistics": CountryStatistics,
+    "key-financial-indicators": KeyFinancialIndicators,
+    "key-financial-indicators-analytics": KeyFinancialIndicatorsAnalytics
+  },
+
+  data() {
+    return {
+      selectedRegions: [],
+      housingType: null,
+      analyticsResult: {},
+      selectedCountries: ["Singapore"],
+      countryStatistics: null,
+      selectedCountryStatisticsFeatures: [
+          "gdp nominal",
+          "unemployment rate",
+          "Financial Development Index",
+          "population density",
+          "literacy rate",
+          "life expectacy (overall)",
+          "gini",
+          "Consumer Price Index, All items",
+      ],
+      chartData: null,
+      tabs: null,
+      panel: [0, 0],
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    }
+  },
+
+    mounted() {
+      this.housingType = this.$route.params.analytics_result.property_type
+      this.selectedRegions.push(this.$route.params.analytics_result.region_found)
+            
+      this.getEverything();
+
+    },
+
+    methods: {
+
+      getEverything() {
+        this.getCountryStatistics()
+        this.getChartData()
+      },
+
+      getCountryStatistics() {
+          // get statistics from aggregate.countries
+          this.countryStatistics = null
+          var url = process.env.BACKEND + "/api/countries/statistics/" + this.selectedCountries
+
+          this.$axios.get(url).then((response) => {
+              this.countryStatistics = response.data
+          })
+      },
+
+      getChartData() {
+        this.chartData = null
+        // this function gets chart data and stores in this.chartData
+        var url = process.env.BACKEND + "/api/charts/" + this.selectedCountries
+
+        this.$axios.get(url).then((response) => {
+            this.chartData = response.data.charts
+        })
+      },
+  }
+
+}
+
+</script>
+
+<style scoped>
+  .file {
+    width:700px;
+    margin-top:15px;
+  }
+
+  .fileName {
+    float:left;
+  }
+
+  .fileResults {
+    float:right;
+    margin:0 auto;
+  }
+
+  .fileResultsSuccess {
+    margin-right:30px;
+  }
+
+  .fileResultsError {
+    margin:0 auto;
+    padding-right:40px;
+  }
+
+  .successDot {
+    height: 25px;
+    width: 25px;
+    background-color: #4BB543;
+    border-radius: 50%;
+    display: inline-block;
+  }
+
+  .parent {
+    margin: 0;
+    width: 100%;
+  }
+
+  .left {
+      float: left;
+      width: 60%;
+      height: 100%;
+      padding-left:50px;
+      padding-right:30px;
+  }
+
+  .right {
+    position: fixed;
+    top: 0%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  .v-expansion-panel::before {
+   box-shadow: none !important;
+  }
+
+  .v-expansion-panel-header {
+   padding:0;
+   margin-top:20px;
+   margin-bottom:20px;
+  }
+
+  .v-expansion-panel-content >>> .v-expansion-panel-content__wrap {
+    padding:0 15px 0 0;
+    margin-bottom:25px;
+  }
+
+  .analyticsIcons {
+    margin-right:10px;
+  }
+
+  .regionTabText {
+    text-transform: none !important;
+    margin-left: 10px;
+    font-size:16px;
+  }
+
+  .featuresTitle{
+    color:#215085 !important;
+    font-weight:700;
+    font-size:16px;
+    padding:16px;
+  }
+
+  .charts  {
+    box-shadow: none !important;
+    padding-left:10px;
+    padding-right:10px;
+  }
+
+  .chartsTitle{
+    color:#d29a42;
+    margin-bottom:10px;
+    text-align:center;
+  }
+
+  .chartsDescription {
+    font-size:13px;
+    padding-left:10px;
+    padding-right:10px;
+  }
+
+  .uploadDate {
+    color:#5a5a5a;
+    font-size:12px;
+  }
+
+  .link {
+    color:#215085;
+    font-size:14px;
+    font-weight:700;
+  }
+
+  .headerText {
+    color:#215085;
+  }
+  
+  .atAGlanceResults {
+    text-align:center;
+    font-size:30px;
+    color:#215085;
+    font-weight:700;
+  }
+
+  .atAGlanceSubHeader {
+    text-align:center;
+    font-size:14px;
+    margin-top:-15px;
+  }
+</style>
